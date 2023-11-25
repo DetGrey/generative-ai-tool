d85d80234680d0dfd35a62cd0f5dd6d06d5c44e9
DetGrey <anenovlar@gmail.com>
2023-11-25 17:26:21 +0100

    Code added

--- main.js
+++ main.js
@@ -1,81 +1,32 @@
-/*
-* Backend:
-* ollama run mistral
-* */
-/*
-const button = document.querySelector('button');
-const input = document.querySelector('input');
-const pTag = document.querySelector('p');
-button.addEventListener("click", () => {
-    fetch("http://localhost:11434/api/generate", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            model: "mistral",
-            prompt: `The following is a list of dishes you can make with the following ingredients: ${input.value}`,
-            stream: false
-        })
-    })
-        .then(response => response.json())
-        .then(data => {
-            pTag.innerText = data.response;
-            console.log(data.response);
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
-});*/
+// ---------------------------------------------------------------------------------------- TOGGLE FILTERS
+const searchToolbar = document.querySelector('#search-toolbar');
+const filtersButton = document.querySelector('#toggle-filters-button');
+filtersButton.addEventListener('click', () => {
+    searchToolbar.firstElementChild.classList.toggle('hidden');
+})
 
-const button = document.querySelector('button');
-const input = document.querySelector('input');
-const pTag = document.querySelector('p');
+// ---------------------------------------------------------------------------------------- TOGGLE LOADING
+const loading = document.querySelector('#loading');
+const description = document.querySelector('#description')
+function toggleHidden() {
+    loading.classList.toggle('hidden');
+}
+
+// ---------------------------------------------------------------------------------------- SEARCH
+const button = document.querySelector('#search');
+const giftOptions = document.querySelector('#gift-options');
 button.addEventListener("click", () => {
+    description.classList.add('hidden');
+    giftOptions.innerText = '';
+    toggleHidden();
 
     getGeneratedText(generateQuery(fetchFilters()))
         .then(generatedText => {
-            pTag.innerText = generatedText;
+            description.classList.remove('hidden');
+            toggleHidden();
+            // giftOptions.innerText = generatedText;
+            styleAndRenderGeneratedText(generatedText);
         });
 });
 
-function generateQuery(filtersObject) {
-    let query = `Create a list of 10 gift options for `;
-    for (const filterKey in filtersObject) {
-        if (filterKey.length > 0) {
-            query += filtersObject[filterKey];
-        }
-    }
-    console.log(query)
-    return query;
-}
-
-function fetchFilters() {
-    const gender = document.querySelector('input[name="gender"]:checked');
-
-    const age = document.querySelector('#age');
-
-    let options = [];
-    const selectOptions = document.querySelectorAll('input[name="extra-options"]:checked');
-    if (selectOptions.length !== 0) {
-        selectOptions.forEach(option => {
-            options.push(option.value);
-        })
-    }
-    else {
-        options = '';
-    }
-
-    const relationship = document.querySelector('#relationship');
-
-    const suggestions = document.querySelector('#user-suggestions');
-
-    return {
-        gender: gender.value !== 'undefined' ? 'someone who is male' + gender.value + '. ' : '',
-        age: age.value.length > 0 ? 'Their age is ' + age.value + '. ' : '',
-        options: options.length > 0 ? 'The gift suggestions should be in one of the following categories: ' + options + '. ' : '',
-        relationship: relationship.value !== 'undefined' ? 'The relation to you is: ' + relationship.value + '. ' : '',
-        suggestions: suggestions.value.length > 0 ? 'Suggestion categories could be ' + suggestions.value + '. ' : '',
-    }
-}
 
--- utils.js
+++ utils.js
@@ -1,5 +1,45 @@
 const HUGGINGFACE_TOKEN = "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";
 
+// ---------------------------------------------------------------------------------------- GENERATE QUERY
+function generateQuery(filtersObject) {
+    let query = `Create a list of 10 gift options for `;
+    for (const filterKey in filtersObject) {
+        if (filterKey.length > 0) {
+            query += filtersObject[filterKey];
+        }
+    }
+    query += ' Only find modern suggestions and please be creative. Please DO NOT mention the same thing twice nor mention very similar options. DO NOT mention: cuff links, cufflinks, tie clip, golf club, luggage.'
+    return query;
+}
+
+// ---------------------------------------------------------------------------------------- FETCH SELECTED FILTERS
+function fetchFilters() {
+    const gender = document.querySelector('#gender');
+    const age = document.querySelector('#age');
+    const relationship = document.querySelector('#relationship');
+    const suggestions = document.querySelector('#user-suggestions');
+    const selectOptions = document.querySelectorAll('input[name="extra-options"]:checked');
+
+    let options = [];
+    if (selectOptions.length !== 0) {
+        selectOptions.forEach(option => {
+            options.push(option.value);
+        })
+    }
+    else {
+        options = '';
+    }
+
+    return {
+        gender: gender.value !== 'undefined' ? 'someone who is ' + gender.value + '. ' : '',
+        age: age.value.length > 0 ? 'Their age is ' + age.value + '. ' : '',
+        options: options.length > 0 ? 'The gift suggestions should be in one of the following categories: ' + options + '. ' : '',
+        relationship: relationship.value !== 'undefined' ? 'The relation to you is: ' + relationship.value + '. ' : '',
+        suggestions: suggestions.value.length > 0 ? 'Suggestion categories could be ' + suggestions.value + '. ' : '',
+    }
+}
+
+// ---------------------------------------------------------------------------------------- GET GENERATED TEXT
 async function getGeneratedText(query) {
     // We can call this function 8 times before we respond
     let counter = 0;
@@ -8,12 +48,12 @@ async function getGeneratedText(query) {
     while (generatedText !== prevGeneratedText && counter < 8) {
         prevGeneratedText = generatedText;
         generatedText = await fetchText(generatedText);
-        console.log(generatedText)
         counter++;
     }
+
     return generatedText;
 }
-
+// ---------------------------------------------------------------------------------------- FETCH TEXT
 function fetchText(query) {
     return fetch(
         "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct",
@@ -30,4 +70,24 @@ function fetchText(query) {
         .then(data => {
             return data[0].generated_text;
         })
+}
+
+// ---------------------------------------------------------------------------------------- STYLE AND RENDER GENERATED TEXT
+function styleAndRenderGeneratedText(text) {
+    const array = text.split('\n').slice(1);
+    for (let option of array) {
+        const a = document.createElement('a');
+        a.classList.add('gift-options');
+        a.href = `https://www.google.com/search?q=${option.split('. ')[1]}`;
+        a.target = '_blank';
+        a.rel = 'noopener noreferrer'; // https://stackoverflow.com/questions/17711146/how-to-open-link-in-a-new-tab-in-html
+        a.innerText = option.toString();
+        giftOptions.appendChild(a);
+
+        const hr = document.createElement('hr');
+        giftOptions.appendChild(hr);
+        console.log(a)
+    }
+    console.log(array)
+    return text;
 }
